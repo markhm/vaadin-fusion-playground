5173828eeecb8dc38a015968b32f7b8255baf3ff
Mark <github@hissinkmuller.nl>
2020-12-12 11:43:27 +0100

    Removed unused test

--- src/test/typescript/cookie-test.ts
+++
@@ -1,9 +0,0 @@
-import { setCookie, getCookie } from "../../../frontend/utils/security";
-
-console.log('something');
-
-setCookie("user_email","bobthegreat@gmail.com",30); //set "user_email" cookie, expires in 30 days
-
-var userEmail = getCookie("user_email");//"bobthegreat@gmail.com"
-
-console.log('getCookie is: ' + userEmail);
