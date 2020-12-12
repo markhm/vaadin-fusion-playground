5734961a9e3c8541b89c02b46ce41fccfda7b6c4
Mark <github@hissinkmuller.nl>
2020-12-12 11:43:20 +0100

    Added first test skeleton

---
+++ src/test/java/fusion/playground/data/service/ResponseServiceTest.java
@@ -0,0 +1,50 @@
+package fusion.playground.data.service;
+
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class ResponseServiceTest
+{
+    @MockBean
+    private static ResponseRepository responseRepository;
+
+    @Autowired
+    private ResponseService responseService;
+
+    @MockBean
+    private static UserService userService;
+
+    @MockBean
+    private static QuestionService questionService;
+
+    @MockBean
+    private static PossibleAnswerService possibleAnswerService;
+
+    @TestConfiguration
+    static class EmployeeServiceImplTestContextConfiguration {
+
+        @Bean
+        public ResponseService answerService() {
+
+            return new ResponseService(responseRepository, userService, questionService, possibleAnswerService);
+        }
+    }
+
+    @Before
+    public void setUp() {
+
+    }
+
+    public void testSomething()
+    {
+    }
+
+
+}
