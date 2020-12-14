914edf0b62cf0bef6c366b781aef7cc6a81e9967
Mark <github@hissinkmuller.nl>
2020-12-14 10:05:48 +0100

    Conditionally activated bean again

--- src/main/java/fusion/playground/data/generator/DataGenerator.java
+++ src/main/java/fusion/playground/data/generator/DataGenerator.java
@@ -5,18 +5,19 @@ import com.vaadin.flow.spring.annotation.SpringComponent;
 import fusion.playground.data.service.PersonRepository;
 import fusion.playground.data.entity.Person;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.vaadin.artur.exampledata.DataType;
 import org.vaadin.artur.exampledata.ExampleDataGenerator;
 
-// @SpringComponent
+@SpringComponent
+@ConditionalOnWebApplication
 public class DataGenerator {
 
-    // @Bean
+    @Bean
     public CommandLineRunner loadData(PersonRepository personRepository) {
         return args -> {
             Logger logger = LoggerFactory.getLogger(getClass());
